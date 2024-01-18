69dfb83cf179544cea97338146831a9fbabc2573
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-01-18 17:17:04 +0500

    Event Handler

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+
+function Message() {
+    const name = 'Saeed Ur Rehman';
+    return <h1>Hello {name}</h1>
+}
+
+export default Message; 
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["New York", "London", "San Fransisco", "Paris"];
+  let selectedIndex = -1;
+
+  // const message = items.length === 0 ? <p>No items found</p> : null;
+
+  // Event Handler
+  const handle = (event: MouseEvent) => console.log(event);
+  return (
+    <>
+      <h1>List</h1>
+      {/* {message} */}
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            } 
+            
+            key={item}
+            onClick={handle}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
