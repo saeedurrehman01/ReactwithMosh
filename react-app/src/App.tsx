b4c697d0701935669d8220481af48bb349b764aa
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-01-21 15:58:52 +0500

    Building a form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,33 +1,11 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Like from "./components/Like";
-import ListGroup from "./components/ListGroup";
-import "bootstrap/dist/css/bootstrap.css";
-import { FaCalendarAlt } from "react-icons/fa";
+import Form from "./components/Form";
 
 function App() {
-  let items = ["New York", "London", "San Fransisco", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <FaCalendarAlt color="Red" size={40} />
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <Alert></Alert>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
-        My Button
-      </Button>
-      <Like onClick={() => console.log("Clicked")} />
+      <Form />
     </div>
   );
 }
 
 export default App;
-
-let arc = 1;
---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
