1c4e24204bd47f092a1f28b04160780d2992e17f
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-01-21 16:05:46 +0500

    Building a form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -9,6 +9,11 @@ const Form = () => {
         </label>
         <input id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">Age</label>
+        <input id="age" type="number" className="form-control" />
+          </div>
+          <button className="btn btn-primary" type="submit">Submit</button>
     </form>
   );
 };
