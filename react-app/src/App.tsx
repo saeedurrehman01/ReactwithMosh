7efeb1a7e22a56827f9f8619b88bbd41ce1a45d3
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2024-01-30 21:53:31 +0500

    Understanding HTTP Requests

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,51 +1,51 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
+// import { useState } from "react";
 
-// interface User {
-//   id: number;
-//   name: string;
-// }
-
-// function App() {
-//   const [users, setUsers] = useState<User[]>([]);
-
-//   useEffect(() => {
-//     axios
-//       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-//       .then((res) => setUsers(res.data));
-//   }, []);
-
-//   return (
-//     <ul>
-//       {users.map((user) => (
-//         <li key={user.id}>{user.name}</li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default App;
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<User[]>([]);
 
-  function decCount() {
-    setCount(prevCount => prevCount - 1);
-  }
-
-  function incCount() {
-    setCount(prevCount => prevCount + 1);
-  }
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
 
   return (
-    <>
-      <button onClick={decCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incCount}>+</button>
-    </>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
 export default App;
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function decCount() {
+//     setCount(prevCount => prevCount - 1);
+//   }
+
+//   function incCount() {
+//     setCount(prevCount => prevCount + 1);
+//   }
+
+//   return (
+//     <>
+//       <button onClick={decCount}>-</button>
+//       <span>{count}</span>
+//       <button onClick={incCount}>+</button>
+//     </>
+//   );
+// }
+
+// export default App;
